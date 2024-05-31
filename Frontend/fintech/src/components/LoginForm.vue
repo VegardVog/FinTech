a20ba2fc2b3ef18ad8354be0a376d348e7deab77
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-05-31 14:49:07 +0200

    Tried context factory but not done

--- Frontend/fintech/src/components/LoginForm.vue
+++ Frontend/fintech/src/components/LoginForm.vue
@@ -47,7 +47,7 @@ export default defineComponent({
 
     const login = async () => {
       store.dispatch("loginUser");
-      //getStockData("S&P500");
+      getStockData("AAPL");
     };
 
     return {
